25de3c2a87c4b8ee674705a0e4d4e6b7562cd793
Hunter <hunterwebbmc@gmail.com>
2021-10-19 20:58:04 -0400

    Updated ball collision to reflect off walls

--- main.py
+++ main.py
@@ -7,19 +7,24 @@
     Python Version: 3.9.5
 """
 
-# Module Imports
-import math
 
+# Module Imports
+
+import random
+import os
 import pygame
 import sys
 from pygame.locals import *
+
 
 # Constants
 
 SCREEN_WIDTH = 400
 SCREEN_HEIGHT = 600
 PADDLE_WIDTH = 100
+BALL_R = 10
 PADDING = 5
+
 
 # Movement Initializers
 
@@ -36,7 +41,7 @@ class Paddle(object):
         self.y = y
 
     def draw(self):
-        pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(math.floor(self.x), math.floor(self.y), PADDLE_WIDTH, 10))
+        pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(int(self.x), int(self.y), PADDLE_WIDTH, 10))
 
     def move(self, d):
         if d and self.x > 0:
@@ -45,28 +50,52 @@ class Paddle(object):
             self.x += 1 / SPEED
 
 
+class Ball(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.vx = random.randint(-5, 5) / 10
+        self.vy = 3/SPEED
+
+    def draw(self):
+        pygame.draw.circle(screen, (155, 155, 255), (int(self.x), int(self.y)), BALL_R)
+
+    def update(self):
+        # self.print_info()
+
+        if self.y >= SCREEN_HEIGHT - BALL_R:
+            pygame.quit()
+            sys.exit()
+
+        if not BALL_R < int(self.x) < SCREEN_WIDTH - BALL_R:
+            self.vx *= -1
+        if not BALL_R < int(self.y) < SCREEN_HEIGHT - BALL_R:
+            self.vy *= -1
+
+        self.y += self.vy / SPEED
+        self.x += self.vx / SPEED
+
+        self.draw()
+
+    def print_info(self):
+        print("x: " + str(self.x), "\ny: " + str(self.y), "\nvx: " + str(self.vx), "\nvy: " + str(self.vy) + "\n" * 20)
+
+
 # Game Initialization
 
 pygame.init()
 screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
 paddle = Paddle((SCREEN_WIDTH - PADDLE_WIDTH) / 2, SCREEN_HEIGHT - PADDING * 10)
+ball = Ball(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
 
 # Game Loop
 
-def move_left():
-    paddle.move(True)
-
-
-def move_right():
-    paddle.move(False)
-
-
-def check_movement():
+def move():
     if L_DOWN:
-        move_left()
+        paddle.move(True)
     elif R_DOWN:
-        move_right()
+        paddle.move(False)
 
 
 while True:
@@ -85,8 +114,9 @@ while True:
             pygame.quit()
             sys.exit()
 
-    check_movement()
+    move()
 
     screen.fill(0)
     paddle.draw()
+    ball.update()
     pygame.display.flip()
