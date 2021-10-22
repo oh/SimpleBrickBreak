4176c62ad4bdeabe594913225ab5cd0a42acf827
Hunter <hunterwebbmc@gmail.com>
2021-10-22 19:58:35 -0400

    Misc changes

--- main.py
+++ main.py
@@ -7,14 +7,12 @@
     Python Version: 3.9.5
 """
 
-
 # Module Imports
 
 import random
 import pygame
 import sys
 from pygame.locals import *
-
 
 # Constants
 
@@ -23,11 +21,10 @@ SCREEN_HEIGHT = 600
 PADDLE_WIDTH = 100
 BALL_R = 10
 PADDING = 1
-BRICK_W = 30
-BRICK_H = 10
+BRICK_W = 80
+BRICK_H = 30
 N_BRICK_W = SCREEN_WIDTH / (BRICK_W + PADDING)
 N_BRICK_H = (SCREEN_HEIGHT / 3) / (BRICK_H + PADDING)
-
 
 # Movement Initializers
 
@@ -59,7 +56,7 @@ class Ball(object):
         self.x = x
         self.y = y
         self.vx = random.randint(-5, 5) / 10
-        self.vy = 10/SPEED
+        self.vy = 10 / SPEED
 
     def draw(self):
         pygame.draw.circle(screen, (155, 155, 255), (int(self.x), int(self.y)), BALL_R)
@@ -70,8 +67,7 @@ class Ball(object):
         # Bottom of the screen
 
         if self.y >= SCREEN_HEIGHT - BALL_R:
-            pygame.quit()
-            sys.exit()
+            screen.blit(pygame.font.SysFont('arial', 30).render('Some Text', False, (255, 0, 0)), (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2))
 
         # All other walls
 
@@ -84,6 +80,22 @@ class Ball(object):
 
         if paddle.rect.collidepoint(ball.x, ball.y + BALL_R):
             self.vy *= -1
+
+            if L_DOWN:
+                if self.vx >= 0:
+                    self.vx -= 0.1
+                else:
+                    self.vx -= 0.1
+            if R_DOWN:
+                if self.vx <= 0:
+                    self.vx += 0.1
+                else:
+                    self.vx += 0.1
+
+            if self.vx > 1:
+                self.vx = 1
+            if self.vx < -1:
+                self.vx = -1
 
         self.y += self.vy / SPEED
         self.x += self.vx / SPEED
@@ -98,12 +110,14 @@ class Brick(object):
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        self.color = (random.randint(5, 255), random.randint(5, 255), random.randint(5, 255))
         self.rect = pygame.Rect(self.x, self.y, BRICK_W, BRICK_H)
 
-    def draw(self, c):
-        pygame.draw.rect(screen, c, self.rect)
+    def draw(self):
+        pygame.draw.rect(screen, self.color, self.rect)
 
     def update(self):
+
         # Brick Collision
 
         if self.rect.collidepoint(ball.x, ball.y - BALL_R) or self.rect.collidepoint(ball.x, ball.y + BALL_R):
@@ -113,19 +127,25 @@ class Brick(object):
             ball.vx *= -1
             brick_array.remove(self)
 
-        self.draw((255, 255, 255))
+        if len(brick_array) == 0:
+            print('none left')
+
+        self.draw()
 
 
 # Game Initialization
 
 pygame.init()
+pygame.display.set_caption("Simple Brick Break")
+pygame.font.init()
 screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
 paddle = Paddle((SCREEN_WIDTH - PADDLE_WIDTH) / 2, SCREEN_HEIGHT - 50)
 ball = Ball(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 brick_array = []
-
-print(int(SCREEN_WIDTH / (N_BRICK_W + PADDING)),
-      int((SCREEN_HEIGHT / 3) / (N_BRICK_H + PADDING)))
+font = pygame.font.SysFont('arial.ttf', 30)
+text = font.render('Some Text', False, (255, 0, 0))
+screen.blit(text, (0, 0))
+pygame.display.update()
 
 for x in range(int(N_BRICK_W) + 1):
     for y in range(int(N_BRICK_H)):
